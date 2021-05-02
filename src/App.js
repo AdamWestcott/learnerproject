f3e7de47579e4b0bb66fc0ad192898db6728dc1c
AdamWestcott <72984379+AdamWestcott@users.noreply.github.com>
2021-05-02 20:22:09 +0100

    added functionality to add new learners to the database from by submitting details from page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import firebase from "./firebase";
 function App() {
 const [learners, setLearners] =useState([]);
 const [loading, setLoading] =useState(false); /* Constant to show when the information is loading*/
+const [firstName, setfirstName] = useState('');
+const [secondName, setsecondName] = useState('');
+const [emailAddress, setemailAddress] = useState('');
+const [score, setScore] = useState('');
 
 const ref = firebase.firestore().collection("learners");
 
@@ -21,21 +25,63 @@ function getLearners() {
   
 }
 
+function addLearner(newlearner){
+  ref
+  .doc()
+  .set(newlearner)
+}
+
 useEffect(() =>{
   getLearners();
 },[]);
 
-if(loading){
+if(loading){ /* if loading is set to true display loading message*/
   return <h1>Loading...</h1>;
 }
 
+
 return (
+  
   <div>
-    <h1>learners</h1>
+    <h1>Learners</h1>
+    <h2>Average Score for Learners:</h2>
+    <h3>Add New Learner</h3>
+    <p>Enter First Name</p>
+    <input
+          type="text"
+          value={firstName}
+          onChange={(e) => setfirstName(e.target.value)}
+        />
+      <h3>Enter Last Name</h3>
+    <input
+          type="text"
+          value={secondName}
+          onChange={(e) => setsecondName(e.target.value)}
+        />
+    <h3>Enter Score</h3>
+    <input
+          type="number"
+          value={score}
+          onChange={(e) => setScore(e.target.value)}
+        />
+<h3>Enter Email Address</h3>
+
+<input
+          type="text"
+          value={emailAddress}
+          onChange={(e) => setemailAddress(e.target.value)}
+        />
+
+    <button onClick={(e) => addLearner({firstName, secondName, score, emailAddress})}>
+    Submit
+    </button>
+
+
     {learners.map((learner) => (
       <div key={learner.id}>
         <h2>{learner.firstName + " " + learner.secondName}</h2>
         <p>{"Score =" + " " + learner.score}</p>
+        <button type="button" class="btn btn-primary">Edit</button>
       </div>
     
     
